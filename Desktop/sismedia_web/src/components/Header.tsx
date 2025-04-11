55a139e509058aab12981124b1ab03e894daaa87
Raquelll77 <raquelalvarado1717@gmail.com>
2025-04-11 08:50:41 -0600

    modificando el header

--- Desktop/sismedia_web/src/components/Header.tsx
+++ Desktop/sismedia_web/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import {Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function Header() {
@@ -10,23 +10,39 @@ export default function Header() {
 
   return (
     <div className="mx-auto">
-      <div className={`flex items-center px-5 pt-2 sm:px-10 md:px-15 shadow-lg`}>
+      <div className="flex items-center justify-between px-5 py-2 sm:px-10 md:px-15 md:pb-2 shadow-lg">
         
-        {/* Logo */}
-        {!isMenuOpen && (
-          <Link to="/">
-            <img
-              className="w-32 sm:w-40 md:w-48"
-              src="LogoSismediaHorizontal.png"
-              alt="logoSismedia"
-            />
-          </Link>
-        )}
+        {/* Logo (ocultar cuando el menú esté abierto en móvil) */}
+        <Link to="/">
+          <img
+            className={`w-32 sm:w-40 md:w-48 ${isMenuOpen ? 'hidden' : 'block'}`}
+            src="LogoSismediaHorizontal.png"
+            alt="logoSismedia"
+          />
+        </Link>
 
-        
+        {/* Icono de menú hamburguesa */}
+        <div className={`md:hidden ${isMenuOpen ? '' : 'block'} absolute top-5 right-5`}>
+          <button onClick={toggleMenu} className="text-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+
         {/* Menú de navegación */}
         <nav
-          className={`md:flex md:gap-8 ${isMenuOpen ? 'block pt-4 mt-8 ' : 'hidden'} sm:block w-full text-center md:justify-end`}
+          className={`md:flex md:gap-8 ${isMenuOpen ? 'block pt-4 mt-8' : 'hidden'} sm:block w-full text-center md:justify-end`}
         >
           <NavLink
             to="/"
@@ -69,25 +85,6 @@ export default function Header() {
             Contáctanos
           </NavLink>
         </nav>
-
-        {/* Icono de menú hamburguesa (siempre visible en móvil) */}
-        <div className="md:hidden absolute top-2 right-5">
-          <button onClick={toggleMenu} className="text-black">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
       </div>
     </div>
   );
